95cc9eeeef4dba27dc952b29e82f8f8d5dfccd70
Sergio Perez <sperez4mba@gmail.com>
2014-06-03 09:55:02 -0500

    First test/implementation for Ai class didn't make sense. Starting over.

--- lib/ai.rb
+++ lib/ai.rb
@@ -2,12 +2,5 @@ require 'board'
 require 'game_rules'
 
 class Ai
-  def minimax(board, current_depth, maximizing_player)
-    start_diagonal = 6
-    squares = board.squares
-    
-    if squares[start_diagonal - 2] == squares[start_diagonal - 4]
-      return start_diagonal
-    end
-  end
+  
 end
--- spec/ai_spec.rb
+++ spec/ai_spec.rb
@@ -2,17 +2,5 @@ require 'ai'
 require 'board'
 
 describe Ai do
-  let(:ai){ described_class.new } 
-  let(:board){ Board.new(9) }
-  it "computer can win in one move and returns winning move" do
-    board.squares[0] = 'X'
-    board.squares[1] = 'X'
-    board.squares[2] = 'O'
-    board.squares[4] = 'O'
-    board.squares[8] = 'X'
-    depth = 0
-    maximizing_player = true
-    
-    expect(ai.minimax(board, depth, maximizing_player)).to eql(6)
-  end
+   
 end
