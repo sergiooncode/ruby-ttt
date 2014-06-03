5c9926bb5c257ad5c132eb4b2e71332fd44e50c8
Sergio Perez <sperez4mba@gmail.com>
2014-06-03 11:14:11 -0500

    Added first test to Ai class.

--- lib/ai.rb
+++ lib/ai.rb
@@ -2,5 +2,16 @@ require 'board'
 require 'game_rules'
 
 class Ai
-  
+  INFINITY = 1000000
+  attr_accessor :game_rules
+
+  def initialize
+    self.game_rules = GameRules.new 
+  end
+
+  def minimax(board, depth, maximizing_player)
+    if @game_rules.gameover?(board)
+      return INFINITY
+    end
+  end
 end
--- spec/ai_spec.rb
+++ spec/ai_spec.rb
@@ -1,6 +1,23 @@
 require 'ai'
 require 'board'
+require 'game_rules'
 
 describe Ai do
-   
+  let(:board){ Board.new(9) }
+  let(:ai){ described_class.new } 
+  it "machine is current player and it won" do
+    board.squares[0] = 'X'
+    board.squares[1] = 'O'
+    board.squares[2] = 'X'
+    board.squares[3] = 'X'
+    board.squares[4] = 'O'
+    board.squares[5] = '-'
+    board.squares[6] = '-'
+    board.squares[7] = 'O'
+    board.squares[8] = '-'
+    depth = 6
+    maximizing_player = true
+
+    expect(ai.minimax(board, depth, maximizing_player)).to eql(1000000)
+  end
 end
