3d8633577047adc986ec3b1bea547fd3a6495674
Sergio Perez <sperez4mba@gmail.com>
2014-06-08 20:31:37 -0500

    Added all methods to make machine player unbeatable.

--- lib/ai.rb
+++ lib/ai.rb
@@ -4,26 +4,19 @@ require 'game_rules'
 class Ai
   INFINITY = 1000000
   
-  attr_accessor :game_rules
-
+  attr_accessor :game_rules, :smart_move
+  
   def initialize
-    self.game_rules = GameRules.new 
+    @game_rules = GameRules.new 
+    @smart_move = -1
   end
 
-  def heuristic(board, maximizing_player)
-    if !@game_rules.tie?(board)
-      if maximizing_player
-        if @game_rules.who_is_winner == 'X'
-          score = -INFINITY
-        else
-          score = INFINITY
-        end
+  def heuristic(board)
+    if !game_rules.tie?(board)
+      if game_rules.who_is_winner == 'X'
+        score = -INFINITY
       else
-        if @game_rules.who_is_winner == 'X'
-          score = INFINITY
-        else
-          score = -INFINITY
-        end
+        score = INFINITY
       end
     else
       score = 0
@@ -41,10 +34,51 @@ class Ai
     moves
   end
 
-  def minimax(board, depth, maximizing_player)
-    if @game_rules.gameover?(board)
-      score = heuristic(board, maximizing_player)
+  def board_with_new_move(old_board, move, token)
+    board = Board.new(9)
+    board.squares = old_board.squares.dup
+    board.squares[move] = token 
+    board
+  end
+
+  def maximizing_branch(next_board, depth)
+    moves = possible_moves(next_board)
+    best_score = -INFINITY
+    best_move = -1
+    moves.each do |move|
+      score = minimax(board_with_new_move(next_board, move, 'O'), depth + 1, false)
+      if score > best_score
+        best_move = move
+      end
+      best_score = [score, best_score].max
+    end
+    @smart_move = best_move
+    best_score
+  end
+
+  def minimizing_branch(next_board, depth)
+    moves = possible_moves(next_board)
+    best_score = INFINITY
+    best_move = -1
+    moves.each do |move|
+      score = minimax(board_with_new_move(next_board, move, 'X'), depth + 1, true)
+      if score < best_score
+        best_move = move
+      end
+      best_score = [score, best_score].min
+    end
+    best_score
+  end
+
+  def minimax(next_board, depth, maximizing_player)
+    if game_rules.gameover?(next_board) || depth == 9
+      score = heuristic(next_board)
       return score
+    end
+    if maximizing_player
+      maximizing_branch(next_board, depth)
+    else
+      minimizing_branch(next_board, depth)
     end
   end
 end
--- spec/ai_spec.rb
+++ spec/ai_spec.rb
@@ -11,14 +11,14 @@ describe Ai do
       depth = 6
       maximizing_player = true
 
-      expect(ai.minimax(board, depth, maximizing_player)).to eql(1000000)
+      expect(ai.minimax(board, depth, maximizing_player)).to eql 1000000
     end
     it "returns score when human is current player and it won" do
       board.squares = "XO-X-OX--"
       depth = 5
       maximizing_player = false
 
-      expect(ai.minimax(board, depth, maximizing_player)).to eql(1000000)
+      expect(ai.minimax(board, depth, maximizing_player)).to eql -1000000 
     end
   end
   context "when it is tie" do
@@ -29,7 +29,7 @@ describe Ai do
       depth = 9
       maximizing_player = true
 
-      expect(ai.minimax(board, depth, maximizing_player)).to eql(0)
+      expect(ai.minimax(board, depth, maximizing_player)).to eql 0
     end
   end
   context "when current player loses" do
@@ -40,14 +40,14 @@ describe Ai do
       depth = 5
       maximizing_player = true
 
-      expect(ai.minimax(board, depth, maximizing_player)).to eql(-1000000)
+      expect(ai.minimax(board, depth, maximizing_player)).to eql -1000000
     end
     it "returns score when human is current player and it lost" do
       board.squares = "XOXXO--O-"
       depth = 6
       maximizing_player = false
       
-      expect(ai.minimax(board, depth, maximizing_player)).to eql(-1000000)
+      expect(ai.minimax(board, depth, maximizing_player)).to eql 1000000
     end
   end
   context "when board is not in a terminal case" do
@@ -60,6 +60,49 @@ describe Ai do
       board.squares = "XOX-OXOXO" 
       
       expect(ai.possible_moves(board)).to eql [3]
+    end
+    it "returns score for initial board XOXOO-X-X" do
+      board.squares = "XOXOO-X-X"
+      depth = 7
+      maximizing_player = true
+
+      expect(ai.minimax(board, depth, maximizing_player)).to eql 1000000
+    end
+    it "returns score for initial board XOX-OX---" do
+      board.squares = "XOX-OX---"
+      depth = 5
+      maximizing_player = true
+
+      expect(ai.minimax(board, depth, maximizing_player)).to eql 1000000
+    end
+    it "returns score for initial board XO-X-----" do
+      board.squares = "XO-X-----"
+      depth = 3
+      maximizing_player = true
+
+      expect(ai.minimax(board, depth, maximizing_player)).to eql -1000000
+    end
+    it "returns score for initial board X--------" do
+      board.squares = "X--------"
+      depth = 1
+      maximizing_player = true
+
+      expect(ai.minimax(board, depth, maximizing_player)).to eql 0
+    end
+    it "gets smart_move for initial board XOX--OX--" do
+      board.squares = "XOX--OX--"
+      depth = 5
+      maximizing_player = true
+      ai.minimax(board, depth, maximizing_player)
+
+      expect(ai.smart_move).to eql -1
+    end
+    it "gets smart_move for initial board --X------" do
+      depth = 1
+      maximizing_player = true
+      ai.minimax(board, depth, maximizing_player)
+
+      expect(ai.smart_move).to eql 3
     end
   end
 end
