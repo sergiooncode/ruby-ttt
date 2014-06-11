44adb0c4db785d011f0c404663135d25ae9ca5eb
Sergio Perez <sperez4mba@gmail.com>
2014-06-11 09:22:32 -0500

    Added test to confirm return of minimax is independent of maximizing_player when it's a tie.

--- spec/ai_spec.rb
+++ spec/ai_spec.rb
@@ -31,7 +31,7 @@ describe Ai do
 
       expect(ai.minimax(board, depth, maximizing_player)).to eql 0
     end
-    it "returns score when machine is current player and it is a tie" do
+    it "returns score when human is current player and it is a tie" do
       board.squares = "XOXOXXOXO"
       depth = 9
       maximizing_player = false
